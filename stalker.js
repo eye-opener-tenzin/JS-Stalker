8da019beae363581cd9d34da5c3a0b34b24c83b2
eye-opener-tenzin <tmejay.tm@gmail.com>
2020-10-08 19:44:45 -0700

    add stalker html css and js file

---
+++ stalker.js
@@ -0,0 +1,5 @@
+var firstName = prompt('What is your first name?');
+var lastName = prompt('What is your last name?');
+var userAge = prompt('How old are you?');
+console.log(`User name is ${firstName} ${lastName}`)
+console.log(`User age is ${userAge}`)
